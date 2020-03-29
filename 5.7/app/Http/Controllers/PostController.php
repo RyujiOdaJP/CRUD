ad2dcf892749185e36d63622d0e303d0cc98bd91
RyujiOdaJP <ryuji.oda@gmail.com>
2020-03-29 21:26:37 +0900

    added debugbar

--- 5.7/app/Http/Controllers/PostController.php
+++ 5.7/app/Http/Controllers/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->get();
+        \Debugbar::info($posts);
         return view('posts.index', ['posts' => $posts]);
     }
 
