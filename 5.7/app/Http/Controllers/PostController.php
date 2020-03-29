0e4c08d17dab25aa30fa86ae8cd5dfd107756a47
RyujiOdaJP <ryuji.oda@gmail.com>
2020-03-29 22:28:50 +0900

    added pageing tab

--- 5.7/app/Http/Controllers/PostController.php
+++ 5.7/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->get();
+        $posts = Post::latest()->paginate(5);
         \Debugbar::info($posts);
         return view('posts.index', ['posts' => $posts]);
     }
