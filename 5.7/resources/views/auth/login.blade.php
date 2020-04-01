70ca4100de351b7193f10956a6326eb9c611e7a3
RyujiOdaJP <ryuji.oda@gmail.com>
2020-04-01 21:02:38 +0900

    changed template of login

--- 5.7/resources/views/auth/login.blade.php
+++ 5.7/resources/views/auth/login.blade.php
@@ -1,4 +1,7 @@
-@extends('layouts.app')
+@php
+    $title = __('Login');
+@endphp
+@extends('layouts.my')
 
 @section('content')
 <div class="container">
@@ -12,7 +15,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
